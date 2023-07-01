994cc53db8db2c286bad8e5840837e2c300a747d
hongsh429 <shh0429@kakao.com>
2023-07-01 14:35:13 +0900

    first commit

---
+++ src/main/java/com/sparta/lv3assignment/Lv3AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.lv3assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv3AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lv3AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/lv3assignment/Lv3AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lv3assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv3AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
